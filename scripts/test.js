dab61fa4e4f4bec492aabc4df6640ade3c6293aa
nickeniklas <savonhen@arcada.fi>
2025-01-22 11:47:39 +0200

    landing page

---
+++ scripts/test.js
@@ -0,0 +1,7 @@
+console.log("test.js")
+
+let bigButton = document.querySelector('.ordernow-big')
+
+bigButton.addEventListener('click', ()=> {
+    alert("Sorry Sir, you have been hacked💀💀💀💀💀...")
+})
