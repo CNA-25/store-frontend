46a1c1e04a944b3c6d306db6b5d39889df5dd19e
Nickeniklas <savonhen@arcada.fi>
2025-02-21 18:29:47 +0200

    add from wishlist to cart, other small things...

--- scripts/addToWishlist.js
+++ scripts/addToWishlist.js
@@ -3,7 +3,6 @@ console.log("addToWishlist.js")
 // Add eventlisteners to ADD to WISHLIST buttons
 document.querySelectorAll(".btn-add-wishlist").forEach(button => {
     button.addEventListener("click", function() {
-        console.log("MEGA CLICK")
         const beerDiv = this.closest(".beer-container").querySelector(".flexed-beers")
         const wlBeer = "WL-" + beerDiv.id
         if (localStorage.getItem(wlBeer)){
@@ -19,38 +18,7 @@ document.querySelectorAll(".btn-add-wishlist").forEach(button => {
         }
     })
 })
-/*
 
-// front page beers as add to shopping cart/buy buttons
-saunaSessionAle = document.querySelector('#saunaSessionAle')
-midsummerWheat = document.querySelector('#midsummerWheat')
-midnightBlackIPA = document.querySelector('#midnightBlackIPA')
-
-// list of beers so that we can add easily more beers as nessecary
-const beers = [saunaSessionAle, midsummerWheat, midnightBlackIPA]
-
-// add beer to wishlist ( LOCAL STORAGE )
-// function for adding eventlisteners to "add to wish list" text
-function beerButtons(beerName) {
-    beerName.addEventListener('click', ()=> {
-        console.log("click on " + beerName.id)
-        // add a beer to the localstorage, else create the localstorage
-        if (localStorage.getItem(beerName.id)){
-            // handle localstorage
-            localStorage.setItem(beerName.id, Number(localStorage.getItem(beerName.id)) + 1) 
-            //handle toast to notify user of adding to cart
-            handleToast(beerName)
-        }
-        else {
-            localStorage.setItem(beerName.id, 1) 
-            //handle toast to notify user of adding to cart
-            handleToast(beerName)
-        }
-    })    
-}
-// loop through beers and use function on them 
-beers.forEach(beerButtons)
-*/
 // FUNCTION FOR HANDLE TOAST NOTIFICATION FOR ADDING TO CART
 function handleToast(beer) {
     const toastTrigger = beer
--- scripts/wishlist.js
+++ scripts/wishlist.js
@@ -21,17 +21,15 @@ function createCartItem(title, content, amount) {
     bsList.appendChild(listItem)
 }
 
-// ADD ALL ITEMS FROM LOCAL STORAGE TO LIST
-
 //  localstorage keys: all beers as strings (HARDCODED) 
 const wishlistBeers = ["WL-saunaSessionAle", "WL-midsummerWheat", "WL-midnightBlackIPA"]
 
-// function getting the cart items from the local storage and createCartItem with them
+// FUNCTION FOR GETTING LOCALSTORAGE AND CREATING WITH createCartItem()
 function addWishlistItems(wishlistBeers) {
     wishlistBeers.forEach(beer => {
         if (localStorage.getItem(beer)) {
             storagedBeer = localStorage.getItem(beer)
-            createCartItem(beer, 100 + "€", storagedBeer)
+            createCartItem(beer.slice(3), 100 + "€", storagedBeer)
             console.log(storagedBeer)
         }
     })    
@@ -45,3 +43,33 @@ emtpyCartBtn.addEventListener('click', ()=> {
     ["WL-saunaSessionAle", "WL-midsummerWheat", "WL-midnightBlackIPA"].forEach(item => localStorage.removeItem(item))
     location.reload()
 })
+
+// ADD ALL wlBeer TO CART
+document.querySelector('#add-all-wishlist-btn').addEventListener('click', ()=> {
+    // loop through all beers and get if exsist in local storage
+    ["WL-saunaSessionAle", "WL-midsummerWheat", "WL-midnightBlackIPA"].forEach((wlBeer) => {
+        if (localStorage.getItem(wlBeer)) {
+            // remove wl- from name and add to localstorage (cart)
+            cartBeer = wlBeer.slice(3)
+            cartBeerVal = Number(localStorage.getItem(cartBeer))
+            wlBeerVal = Number(localStorage.getItem(wlBeer))
+            if (localStorage.getItem(cartBeer)) {
+                // item already exsists in cart, add more
+                localStorage.setItem(cartBeer, cartBeerVal + wlBeerVal)
+            } else localStorage.setItem(cartBeer, wlBeerVal)
+            // remove items from wishlist
+            localStorage.removeItem(wlBeer)
+            // show results /refresh page
+            location.reload()
+        }
+    })     
+})
+
+// ADD ONE wlBeer TO CART
+
+
+// TO DO:
+// - remove invidual items
+// - modal to added to wishlist, not cart
+// - add to cart buttons
+// - toast pop up message
