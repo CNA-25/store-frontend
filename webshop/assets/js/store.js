69a80492cc6016d60eb3799c009935158b285b8b
haanpaas <haanpaas@arcada.fi>
2025-03-03 11:54:30 +0200

    store html and js added

---
+++ webshop/assets/js/store.js
@@ -0,0 +1,34 @@
+async function showProducts() {
+    const URL = 'https://product-service-cna-product-service.2.rahtiapp.fi/products';
+    try {
+        await fetch(URL);
+    } catch (error) {
+        console.log('Failed to fetch api', error);
+        document.querySelector('#output').innerHTML = "<p>Failed to fetch API</p>";
+        return;
+    }
+    const resp = await fetch(URL);
+    const data = await resp.json();
+    console.log(data.msg);
+
+    // Visa produkter
+    var outputString = ``;
+
+    for (product of data.products) {
+        outputString += `
+            <div id="${product.sku}">
+                <p>----------</p>
+                <p>${product.name}</p>
+                <img src="${product.image}" alt="Product Image" >
+                <p>${product.price}€</p>
+                <p>Info:</p>
+                <p>${product.description}</p>
+                <p>${product.category}, ${product.country}</p>
+                <button onclick="addItemToCart(${product.id})">Add to Cart</button>
+                <button onclick="addItemToWishlist(${product.sku})">Add to Wishlist</button>
+            <div>
+        `;
+    }
+    document.querySelector('#products').innerHTML = outputString;
+}
+showProducts();
